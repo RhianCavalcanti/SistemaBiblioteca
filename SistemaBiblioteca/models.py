10cb803493a5dbbeaee501167eacde5ca8dc33d7
RhianCavalcanti <71891460+RhianCavalcanti@users.noreply.github.com>
2021-07-28 19:29:42 -0300

    Update models.py

--- SistemaBiblioteca/models.py
+++ SistemaBiblioteca/models.py
@@ -1,9 +1,19 @@
+import time
+from pymongo import MongoClient
+import random
+client= MongoClient('mongodb+srv://projeto:projeto@sistemabiblioteca.3o1zg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db=client["SistemaBiblioteca"]
+col= db["Livros"]
+col2=db["ADM's"]
+col3=db["Visitantes"]
+
 class Livro:
-    def __init__(self,nome,autor,ano,genero):
+    def __init__(self,nome,autor,ano,genero,admreference):
         self.nome=nome
         self.autor=autor
         self.ano=ano
         self.genero=genero
+        self.admreference=admreference
     def setNome(self,nome):
         self.nome=nome
     def setAutor(self,autor):
@@ -12,6 +22,8 @@ class Livro:
         self.ano=ano
     def setGenero(self,genero):
         self.genero=genero
+    def setAdmreference(self,admreference):
+        self.admreference=admreference
     def getNome(self):
         return self.nome
     def getAutor(self):
@@ -20,6 +32,8 @@ class Livro:
         return self.ano
     def getGenero(self):
         return self.genero
+    def getAdmreference(self):
+        return self.admreference
 
 class Adm:
     def __init__(self,login,senha):
@@ -40,3 +54,32 @@ class Visitante:
         self.nome = nome
     def getNome(self):
         return self.nome
+
+def addAdm(login, senha):
+    adms = {"Login": login, "Senha": senha}
+    col2.insert(adms)
+    print("Adm Cadastrado com Sucesso!")
+def addLivro(nome,autor,ano,genero,admreference):
+    conjid = tuple(range(99999, 999998))
+    id = str(random.sample(conjid, 1))
+    verify = col.find({"_id": id})
+    for y in verify:
+        while y == id:
+            conjid = tuple(range(99999, 999998))
+            id = str(random.sample(conjid, 1))
+        if y != id:
+            break
+    livros = {'_id': id, 'Nome do Livro': nome, 'Autor': autor, 'Ano': ano,
+              'Gênero': genero, 'Adicionado por':admreference}
+    col.insert_one(livros)
+    print("Livro Cadastrado com Sucesso!")
+def editLivro(pesquisaed, categoria, infonova):
+    col.update_one({"Nome do Livro": pesquisaed}, {"$set": {categoria: infonova}})
+def excluirLivro(nome):
+    col.delete_one({"Nome do Livro": nome})
+def pesquisaLivro(categoria, nomepesquisa):
+    docpesquisa = col.find({categoria: nomepesquisa})
+    for i in docpesquisa:
+        print(i)
+def addVisit(nome):
+    col3.insert_one({"Nome": nome, "Hora e dia de entrada": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())})
